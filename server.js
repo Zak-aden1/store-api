11fe0cbaa9a45709c03d69277a0de23a35d1e2a5
zak aden <s.aden159@gmail.com>
2022-07-24 13:50:29 +0100

    set up middleware

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ require('dotenv').config()
 const express = require('express');
 
 const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+ res.send('store api')
+})
+
+app.listen(process.env.PORT)
