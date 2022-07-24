db20c346dcef064e03500ae8c9652fefde273313
zak aden <s.aden159@gmail.com>
2022-07-24 18:13:45 +0100

    make getAll handle query params for filtering

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,26 @@
-const getAllProducts = (req, res) => {
-  throw new Error('testing async err')
-  res.status(200).json({data: 'get all products'})
+const Product = require('../models/product')
+const mongoose = require('mongoose')
+
+const getAllProducts = async (req, res) => {
+  // throw new Error('testing async err')
+  const {featured, company, name } = req.query
+
+  const queryObject = {}
+
+  if(featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if(company) {
+    queryObject.company = company
+  }
+  if(name) {
+    queryObject.name = {$regex: name, $options: 'i'}
+  }
+
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject)
+  res.status(200).json({products})
 }
 
 module.exports = {
--- populate.js
+++ populate.js
@@ -8,7 +8,7 @@ const jsonProducts = require('./products.json');
 connectDB(process.env.MONGO_URI)
   .then(() => {
     Product.create(jsonProducts)
-    .then(() => console.log('success'))
+    .then(() => {console.log('success'); process.exit(0)})
     .catch(err => console.log(err))
   })
-  .catch(err => console.log(err))
+  .catch(err => {console.log(err); process.exit(1)})
