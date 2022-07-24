69e8f684d5fc8718f5bc50c5e86840da14a1d01e
zak aden <s.aden159@gmail.com>
2022-07-24 18:46:23 +0100

    add sort logic

--- controllers/product.js
+++ controllers/product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const getAllProducts = async (req, res) => {
   // throw new Error('testing async err')
-  const {featured, company, name } = req.query
+  const {featured, company, name, sort } = req.query
 
   const queryObject = {}
 
@@ -19,7 +19,16 @@ const getAllProducts = async (req, res) => {
 
   console.log(queryObject);
 
-  const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+  if(sort) {
+    // products = products.sort(sort)
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result= result.sort('createdAt')
+  }
+  const products = await result
+
   res.status(200).json({products})
 }
 
