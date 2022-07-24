d8a954caf17c8766b558f51ed2d61ee6b5e1fd1f
zak aden <s.aden159@gmail.com>
2022-07-24 19:32:04 +0100

    add pagination logic

--- controllers/product.js
+++ controllers/product.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 
 const getAllProducts = async (req, res) => {
   // throw new Error('testing async err')
-  const {featured, company, name, sort } = req.query
+  const {featured, company, name, sort, fields } = req.query
 
   const queryObject = {}
 
+  // filter
   if(featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
@@ -17,9 +18,8 @@ const getAllProducts = async (req, res) => {
     queryObject.name = {$regex: name, $options: 'i'}
   }
 
-  console.log(queryObject);
-
   let result = Product.find(queryObject)
+  // sort logic
   if(sort) {
     // products = products.sort(sort)
     const sortList = sort.split(',').join(' ')
@@ -27,6 +27,22 @@ const getAllProducts = async (req, res) => {
   } else {
     result= result.sort('createdAt')
   }
+
+  // show only chosen fields by user
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  // limit
+  // if(limit) {
+  //   result = result.limit(Number(limit))
+  // }
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page -1) * limit;
+
+  result = result.skip(skip).limit(limit)
   const products = await result
 
   res.status(200).json({products})
